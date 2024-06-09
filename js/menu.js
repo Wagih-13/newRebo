07c0043de5a5751c4e51a081a268427f7d69b968
ahmed wagih <wagiha498@gmail.com>
2024-06-09 20:55:28 +0300

    last

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,3 @@
-
-var swiper = new Swiper(".Slider-container", {
-  effect: "cards",
-  grabCursor: true,
-  centerdSlides: true,
-  loop: true,
-});
-
 // swiper.changeDirection('vertical');
 
 async function fetchData() {
@@ -73,6 +65,12 @@ async function getData() {
   }
 
   document.getElementById("test").innerHTML = htmlTagsContainer;
+  let swiper = new Swiper(".Slider-container", {
+    effect: "cards",
+    grabCursor: true,
+    centerdSlides: true,
+    loop: true,
+  });
 }
 
 getData();
